b2f685f9b4a55f485d27a8ebe5aa618a7b8a62d9
Raphael <raphael.hauk@racing.tuwien.ac.at>
2020-05-15 18:51:39 +0200

    adapt protocol

--- index.js
+++ index.js
@@ -15,40 +15,67 @@ wss.on("connection", function connection(ws) {
         switch (parsed.type) {
             case "connected":
                 client.id = parsed.id;
-                var peers = clients.filter(p => null != p.id && client.id != p.id).map(p => p.id);
-                if (peers.length) {
-                    client.ws.send(JSON.stringify({ type: "peers", peers }));
-                }
-            case "offer":
-                var { offeringPeer, acceptingPeer, offer,
-                    connectionId } = parsed;
+                break;
+            case "initialize_connection":
+                var { offer,
+                    connectionId, timeout } = parsed;
                 connections[connectionId] = {
                     id: connectionId,
-                    offeringPeer: offeringPeer,
-                    acceptingPeer: acceptingPeer,
-                    offer: offer
+                    offeringPeer: client.id,
+                    offer: offer,
+                    requested: false,
+                    expires: (+new Date()) + timeout,
+                    offeringIceCandidates : [],
+                    acceptingIceCandidates : []
                 };
-                var peer = clients.find(v => v.id == acceptingPeer);
-                if (null != peer) {
-                    peer.ws.send(JSON.stringify({
-                        type: "offer",
-                        connectionId,
-                        offeringPeer,
-                        offer
+                client.ws.send(JSON.stringify({
+                    type: "connection_initialized",
+                    connectionId
+                }));
+                break;
+            case "request_connection":
+                var { connectionId } = parsed;
+                var connection = connections[connectionId];
+                if (!connection || connection.requested || connection.expires < +new Date()) {
+                    client.ws.send(JSON.stringify({
+                        type: "connection_not_found",
+                        connectionId
                     }));
                 }
+                else {
+                    connection.requested = true;
+                    connection.acceptingPeer = client.id;
+                    client.ws.send(JSON.stringify({
+                        type: "connection_offer",
+                        connectionId,
+                        offer: connection.offer
+                    }));
+                    for(var candidate of connection.acceptingIceCandidates) {
+                        client.ws.send(JSON.stringify({
+                            type: "new_ice_candidate",
+                            connectionId,
+                            candidate
+                        }));
+                    }
+                }
                 break;
-            case "accept":
-                var { acceptingPeer, accept,
+            case "accept_connection":
+                var { answer,
                     connectionId } = parsed;
                 var connection = connections[connectionId];
+                for(var candidate of connection.offeringIceCandidates) {
+                    client.ws.send(JSON.stringify({
+                        type: "new_ice_candidate",
+                        connectionId,
+                        candidate
+                    }));
+                }
                 var peer = clients.find(v => v.id == connection.offeringPeer);
                 if (null != peer) {
                     peer.ws.send(JSON.stringify({
-                        type: "accept",
+                        type: "connection_accepted",
                         connectionId,
-                        acceptingPeer,
-                        accept
+                        answer
                     }));
                 }
                 break;
@@ -57,6 +84,12 @@ wss.on("connection", function connection(ws) {
                     connectionId } = parsed;
                 var connection = connections[connectionId];
                 var isOffering = connection.offeringPeer === id;
+                if (isOffering) {
+                    connection.offeringIceCandidates.push(candidate);
+                }
+                else {
+                    connection.acceptingIceCandidates.push(candidate);
+                }
                 var peerId = isOffering ? connection.acceptingPeer : connection.offeringPeer;
                 var peer = clients.find(v => v.id === peerId);
                 if (null != peer) {
